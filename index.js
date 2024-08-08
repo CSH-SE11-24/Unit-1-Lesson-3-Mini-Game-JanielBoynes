db8eb6e7f65f1a89c074ed61edff47b8962455f1
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:27:24 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const name="Welcome to the quiz game"
+
+let question1=prompt("whats  drakes new albumn in 2023 called ")
+console.log("If you said For All The Dogs you are correct!")
+  
+ let question2=prompt("what hairstyle does drake wear in 2023")
+console.log("If you said cornrolls you are Correct!!")
+
+let question3=prompt("what is drakes real name")
+console.log("If you said Aubrey,you are correct!")
+parseInt("He is 36 years old")
